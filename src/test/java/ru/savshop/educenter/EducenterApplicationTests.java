cac405d76f5a1924264d2ffdc40fef3bcb8b63d0
Styopa <karen.x.2008@gmail.com>
2018-03-11 23:33:24 +0400

    Start EduCenmter

---
+++ src/test/java/ru/savshop/educenter/EducenterApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.savshop.educenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EducenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
